afca03b967aa223a9521b32eedbc68cfd5ab4481
orlandouchiha425 <valadez425@gmail.com>
2022-02-08 19:33:06 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+// Activity 1
+
+a='bongos';
+b='get your';
+c='';
+
+// activity 2
+console.log('please '+ 'squeeze '+ 'the '+' cheese');
+
+
+// activity 3
+
+console.log(-10>=-100)
+console.log(0<1000);
+console.log(888===889);
+console.log(20<=20);
+
+
+// activity 4
+let num=1;
+while (num<=100) {
+    console.log(num);
+    num++
+    
+}
+
+// let numbers=0
+// while(numbers<=5000){
+// console.log(numbers *numbers)
+// numbers++
+// }
+
+
+
+
+let number=100;
+
+while (number>=1) {
+    console.log(number);
+    number--
+    
+}
+
+
+// activity 5  - commented it out or it will print alot
+// for (let index = 999; index <=9999; index++) {
+//     console.log(index)
+    
+// }
+
+for (let index = 0; index <= 100; index++) {
+    console.log(`This is the song that never ends ${index}`)
+    
+}
+
+// for(let index = 1000; index >=1; index--) {
+
+//     console.log(index)
+    
+// }
